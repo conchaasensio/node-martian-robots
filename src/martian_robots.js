1f75ed31fcf34b88b1686a618d66c54f99752b5d
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-07 12:47:28 +0100

    add turnToRight function

--- src/martian_robots.js
+++ src/martian_robots.js
@@ -1,5 +1,8 @@
 function move(coordenates, initialPosition, instructions) {
   const orientation = initialPosition[4];
+  if (instructions === 'R') {
+    return initialPosition.replace(orientation, turnToRight(orientation));
+  }
 
   return initialPosition.replace(orientation, turnToLeft(orientation));
 }
@@ -10,6 +13,15 @@ function turnToLeft(orientation) {
     S: 'E',
     E: 'N',
     W: 'S',
+  };
+  return leftRotations[orientation];
+}
+function turnToRight(orientation) {
+  const leftRotations = {
+    N: 'E',
+    S: 'W',
+    E: 'S',
+    W: 'N',
   };
   return leftRotations[orientation];
 }
--- src/martian_robots.test.js
+++ src/martian_robots.test.js
@@ -51,3 +51,42 @@ describe('robot turns left', () => {
     expect(position).toBe('0 0 W');
   });
 });
+
+describe('robot turns right', () => {
+  test('robot turns right when the orientation is North', () => {
+    //arrange
+
+    //act
+    const position = move('5 3', '1 1 N', 'R');
+
+    //assert
+    expect(position).toBe('1 1 E');
+  });
+  test('robot turns right when the orientation is East', () => {
+    //arrange
+
+    //act
+    const position = move('5 3', '1 1 E', 'R');
+
+    //assert
+    expect(position).toBe('1 1 S');
+  });
+  test('robot turns right when the orientation is South', () => {
+    //arrange
+
+    //act
+    const position = move('5 3', '1 1 S', 'R');
+
+    //assert
+    expect(position).toBe('1 1 W');
+  });
+  test('robot turns right when the orientation is West', () => {
+    //arrange
+
+    //act
+    const position = move('5 3', '1 1 W', 'R');
+
+    //assert
+    expect(position).toBe('1 1 N');
+  });
+});
