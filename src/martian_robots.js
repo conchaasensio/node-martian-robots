cff96937d504d1f482beebb72bd7342e314dc75e
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-07 12:27:23 +0100

    refactor add function turnToLeft

--- src/martian_robots.js
+++ src/martian_robots.js
@@ -1,13 +1,16 @@
 function move(coordenates, initialPosition, instructions) {
   const orientation = initialPosition[4];
 
+  return initialPosition.replace(orientation, turnToLeft(orientation));
+}
+
+function turnToLeft(orientation) {
   const leftRotations = {
     N: 'W',
     S: 'E',
     E: 'N',
     W: 'S',
   };
-
-  return initialPosition.replace(orientation, leftRotations[orientation]);
+  return leftRotations[orientation];
 }
 module.exports = move;
