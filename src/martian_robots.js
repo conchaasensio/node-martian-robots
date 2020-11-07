3200f68f8a5afd95c890afecef00f597e533fdf6
Concha Asensio <conchaasensiomr@gmail.com>
2020-11-07 12:22:26 +0100

    refactor remove all ifs

--- src/martian_robots.js
+++ src/martian_robots.js
@@ -7,14 +7,7 @@ function move(coordenates, initialPosition, instructions) {
     E: 'N',
     W: 'S',
   };
-  if (orientation === 'N') {
-    return initialPosition.replace(orientation, leftRotations['N']);
-  } else if (orientation === 'W') {
-    return initialPosition.replace(orientation, leftRotations['W']);
-  } else if (orientation === 'S') {
-    return initialPosition.replace(orientation, leftRotations['S']);
-  } else if (orientation === 'E') {
-    return initialPosition.replace(orientation, leftRotations['E']);
-  }
+
+  return initialPosition.replace(orientation, leftRotations[orientation]);
 }
 module.exports = move;
